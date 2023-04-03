6351b1ce2243d075bea1c3e33668011d7b975e3c
jeppjeppjepp0 <jeddmj1@gmail.com>
2023-04-03 16:53:25 -0600

    basic html structure, some css, basic js

---
+++ assets/script.js
@@ -0,0 +1,50 @@
+var title = document.getElementById(".title");
+var question = document.getElementById(".question");
+var answers = document.getElementById(".answers");
+
+
+function displayHome(){
+
+};
+
+function playQuiz(){
+
+};
+
+function addScore(){
+    
+};
+
+function printHighScores(){
+
+};
+
+/*
+display home screen
+    title text
+    highscore nav
+    play quiz button
+    high scores display
+play quiz
+    print questions/assign answers
+        print question
+        create list item button
+    timer
+    scoring
+    save scoring/name
+event listeners for click incorrect/correct
+*/
+
+/*
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+*/
