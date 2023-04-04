64f6be69215ff7c03040076026da374c45c7202f
jeppjeppjepp0 <jeddmj1@gmail.com>
2023-04-03 18:07:09 -0600

    pages link between each other now

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,68 @@
-var title = document.getElementById(".title");
-var question = document.getElementById(".question");
-var answers = document.getElementById(".answers");
+// screens for display
+var homeScreen = document.querySelector("#home-screen");
+var quizScreen = document.querySelector("#quiz-screen");
+var scoreScreen = document.querySelector("#score-screen");
 
+// individual elements
+var title = document.querySelector(".title");
+var scoreBtn = document.querySelector("#score-btn");
+var playQuizBtn = document.querySelector("#play-quiz");
+
+var question = document.querySelector(".question");
+var answers = document.querySelector(".answers");
+
+var highScores = document.querySelector("#high-scores");
+var goHomeButton = document.querySelector("#return-home");
+
+
+function hideAll(){
+    homeScreen.setAttribute("style", "display: none");
+    quizScreen.setAttribute("style", "display: none");
+    scoreScreen.setAttribute("style", "display: none");
+};
 
 function displayHome(){
+    hideAll();
 
+    homeScreen.setAttribute("style", "display: contents");
+
+    title.textContent = "Coding Quiz";
+    scoreBtn.textContent = "View High Scores";
+    playQuizBtn.textContent = "Play Quiz!";
 };
 
 function playQuiz(){
-
+    hideAll();
+    quizScreen.setAttribute("style", "display: contents");
+    console.log("here");
 };
 
-function addScore(){
+function addScore(event){
     
 };
 
 function printHighScores(){
+    hideAll();
 
+    scoreScreen.setAttribute("style", "display: contents");
+
+    goHomeButton.textContent = "Return Home";
 };
+
+hideAll();
+
+displayHome();
+
+playQuizBtn.addEventListener("click", playQuiz);
+scoreBtn.addEventListener("click", printHighScores);
+goHomeButton.addEventListener("click", displayHome);
 
 /*
 display home screen
     title text
     highscore nav
     play quiz button
-    high scores display
+high scores display
 play quiz
     print questions/assign answers
         print question
